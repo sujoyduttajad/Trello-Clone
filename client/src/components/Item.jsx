2de9c462b8f9fbe6d55d74e77a2ae3de9fcc3ebf
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-07 18:53:40 +0530

    Added item-types and Item component

---
+++ client/src/components/Item.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment, useState, useRef } from 'react';
+import { useDrop, useDrag } from "react-dnd"
+import Window from "./Window"
+import ITEM_TYPE from "../data/types"
+
+const Item = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Item
