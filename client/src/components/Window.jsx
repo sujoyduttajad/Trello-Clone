904938f94bfc4ebda58235c9a1fc5bccdc922f5a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-10 20:03:08 +0530

    Create Window.jsx

---
+++ client/src/components/Window.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const Window = ({ show, onClose, item }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Window
