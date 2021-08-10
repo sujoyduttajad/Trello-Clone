4bcb6d443b175730fd334b373fd4bdfb77f5aecf
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-10 20:27:15 +0530

    Update Window.jsx

--- client/src/components/Window.jsx
+++ client/src/components/Window.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Modal from 'react-modal';
 
+Modal.setAppElement('#app');
+
 const Window = ({ show, onClose, item }) => {
     return (
-        <div>
-            
-        </div>
+        <Modal
+            isOpen={show}
+            onRequestClose={onClose}
+            className={'modal'}
+            overlayClassName={"overlay"}
+        >
+            <div className={"close-btn-ctn"}>
+                <h1 style={{ flex: "1 90%" }}>{item.title}</h1>
+                <button 
+                    className={'close-btn'}
+                    onClick={onClose}
+                >X</button>
+            </div>
+            <div></div>
+        </Modal>
     )
 }
 
