c0629c02e99f2b0f3a383e7474b3228d538b4124
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-17 11:31:36 +0530

    Update Homepage.jsx

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -7,9 +7,17 @@ import { data, statuses } from "../data";
 const Homepage = () => {
     const [items, setItems] = useState(data);
 
-    // const onDrop = {item, monitor, status} => {
-
-    // }
+    // when you drop things into different columns, the below code will change the statuses of the items 
+    const onDrop = { item, monitor, status } => {
+        // we need to find the status of the statuses that are passed in
+        const mapping = statuses.find(si => si.status === status);
+        
+        setItems(prevState => {
+            // filter to get the iso filter out the item that we're currently droppping
+            const newItems = prevState
+                .filter(i => i.id !== item.id)
+        })
+    }
 
     return (
         <div>
